f0b69080b3dd3d555ad555bb202a559d3c2c12d7
ggh93234999 <ggh93234999@gmail.com>
2018-06-27 00:12:53 +0800

    revise the tables

--- databases/models.py
+++ databases/models.py
@@ -1,32 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
 class User_profiles(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name = 'user_info')
-    name = models.CharField(max_length = 20)
-    gender = models.CharField(max_length = 5)
+    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name = 'profile')
+    name = models.CharField(max_length = 50, default='noname')
+    gender = models.IntegerField(default = 0)
+    role_id = models.IntegerField(default = 0)
+    is_confirmation = models.BooleanField(default = False)
+    token = models.CharField(max_length=50, default = 'QAQ')
 
 class Events(models.Model):
     name = models.CharField(max_length = 100)
-    team_max = models.DecimalField(decimal_places = 0, max_digits = 5)
-    member_min = models.DecimalField(decimal_places = 0, max_digits = 3)
-    time = models.DateTimeField()
+    description = models.TextField(default = 'qaq')
+    rule = models.TextField(default = 'qaq')
+    status = models.IntegerField(default = 0)
+    excerpt = models.TextField(default = 'qaq')
+    team_max = models.IntegerField(default = 10)
+    member_min = models.IntegerField(default = 1)
+    regist_start = models.DateTimeField(default=datetime.now, blank=True) 
+    regist_end = models.DateTimeField(default=datetime.now, blank=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(default=datetime.now,blank = True)
 
 class Teams(models.Model):
-    name = models.CharField(max_length = 20)
+    name = models.CharField(max_length = 30)
     event_id = models.ForeignKey(Events, on_delete = models.CASCADE)
+    vertify = models.BooleanField(default = False)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True,blank = True)
 
 class Teammembers(models.Model):
     team_id = models.ForeignKey(Teams, on_delete = models.CASCADE)
     user_id = models.ForeignKey(User, on_delete = models.CASCADE)
 
 class Announcements(models.Model):
-    title = models.CharField(max_length = 100)
-    description = models.CharField(max_length = 5000)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    title = models.CharField(max_length = 60)
+    description = models.TextField(default = 'qaq')
+    user_id = models.IntegerField(default = 0)
+    status = models.IntegerField(default = 0)
+    announce_start = models.DateTimeField(default=datetime.now, blank=True)
+    announce_end = models.DateTimeField(default=datetime.now, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- databases/serializers.py
+++ databases/serializers.py
@@ -2,13 +2,31 @@ from rest_framework import serializers
 from databases.models import *
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    name = serializers.CharField(source = 'user_info.name')
-    gender = serializers.CharField(source = 'user_info.gender')
+class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ('username', 'password', 'email', 'name', 'gender')
+        fields = ('id','username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}, 'id':{'read_only':True}}
+    
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.item():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
 
 class User_profilesSerializer(serializers.ModelSerializer):
     class Meta:
