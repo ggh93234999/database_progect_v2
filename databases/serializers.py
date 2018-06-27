ce4733947e5550df5f2a97a8427a99ba119f7bc3
Lin Hsiang <ggh93234999@gmail.com>
2018-06-27 17:46:13 +0800

    something change maybe?

--- databases/serializers.py
+++ databases/serializers.py
@@ -15,13 +15,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id','username', 'email', 'password','is_active')
-        extra_kwargs = {'password': {'write_only': True}, 'id':{'read_only':True}, 'is_active':{'read_only':True}}
+        extra_kwargs = {'password': {'write_only': True}, 'id':{'read_only':True}}
     
     def create(self, validated_data):
         user = User(
             email=validated_data['email'],
             username=validated_data['username'],
-            is_active=False
+            is_active=validated_data['is_active']
         )
         user.set_password(validated_data['password'])
         user.save()
